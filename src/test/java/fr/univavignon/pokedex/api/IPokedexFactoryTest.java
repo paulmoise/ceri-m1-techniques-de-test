8711e419a78700fdbab8827e829d176befe6c78e
paulmoise <m.p.gangbadja@gmail.com>
2024-03-19 22:37:38 +0100

    updated IPokedexFactoryTest

--- src/test/java/fr/univavignon/pokedex/api/IPokedexFactoryTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokedexFactoryTest.java
@@ -1,64 +1,65 @@
 package fr.univavignon.pokedex.api;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 public class IPokedexFactoryTest {
+
+    private IPokemonMetadataProvider metadataProvider;
+    private IPokemonFactory pokemonFactory;
+    private IPokedexFactory pokedexFactory;
+    @BeforeEach
+    void setUp() {
+        metadataProvider = mock(IPokemonMetadataProvider.class);
+        pokemonFactory = mock(IPokemonFactory.class);
+
+        pokedexFactory = new IPokedexFactoryImpl();
+    }
 
     @Test
     void testCreatePokedex() {
         IPokemonMetadataProvider metadataProvider = mock(IPokemonMetadataProvider.class);
         IPokemonFactory pokemonFactory = mock(IPokemonFactory.class);
+        IPokedexFactoryImpl factory = new IPokedexFactoryImpl();
 
-        IPokedexFactory factory = mock(IPokedexFactory.class);
-
-        IPokedex iPokedex = mock(IPokedex.class);
-
-        when(factory.createPokedex(metadataProvider, pokemonFactory)).thenReturn(iPokedex);
+//        IPokedexFactory factory = mock(IPokedexFactory.class);
 
         IPokedex actualPokedex = factory.createPokedex(metadataProvider, pokemonFactory);
 
         assertNotNull(actualPokedex, "The created Pokedex should not be null.");
-        assertEquals(actualPokedex, iPokedex, "The created Pokedex should match the expected Pokedex.");
 
     }
+
 
     @Test
-    void testCreatePokedexWithSpecificMetadataAndPokemon() throws PokedexException {
-        // Création des mocks pour IPokemonMetadataProvider et IPokemonFactory
-        IPokemonMetadataProvider metadataProvider = mock(IPokemonMetadataProvider.class);
-        IPokemonFactory pokemonFactory = mock(IPokemonFactory.class);
-        IPokedexFactory factory = mock(IPokedexFactory.class);
-        IPokedex pokedex = mock(IPokedex.class);
+    void testCreatePokedexUsesDependenciesCorrectly() throws PokedexException {
 
-        // Configuration des comportements des mocks
-        PokemonMetadata metadata = new PokemonMetadata(0, "Bulbizarre", 126, 126, 90);
-        when(metadataProvider.getPokemonMetadata(0)).thenReturn(metadata);
+        PokemonMetadata expectedMetadata = new PokemonMetadata(0, "Bulbasaur", 45, 49, 45);
+        when(metadataProvider.getPokemonMetadata(0)).thenReturn(expectedMetadata);
 
-        Pokemon bulbasaur = new Pokemon(0, "Bulbizarre", 126, 126, 90, 431, 35, 100, 25, 0.75);
+        Pokemon expectedPokemon = new Pokemon(0, "Bulbasaur", 45, 49, 45, 100, 100, 100, 0, 1);
+        when(pokemonFactory.createPokemon(anyInt(), anyInt(), anyInt(), anyInt(), anyInt())).thenReturn(expectedPokemon);
 
+        IPokedex pokedex = pokedexFactory.createPokedex(metadataProvider, pokemonFactory);
 
-        when(pokemonFactory.createPokemon(0, 431, 35, 100, 25)).thenReturn(bulbasaur);
+        assertNotNull(pokedex, "Pokedex should not be null.");
 
-        when(factory.createPokedex(metadataProvider, pokemonFactory)).thenReturn(pokedex);
-        when(pokedex.size()).thenReturn(1);
-        when(pokedex.getPokemon(0)).thenReturn(bulbasaur);
+        PokemonMetadata metadata = pokedex.getPokemonMetadata(0);
+        assertNotNull(metadata, "Metadata should not be null.");
+        assertEquals("Bulbasaur", metadata.getName(), "The name should be Bulbasaur.");
+        verify(metadataProvider).getPokemonMetadata(0);
 
-        // Création du Pokedex via la factory
-        IPokedex actualPokedex = factory.createPokedex(metadataProvider, pokemonFactory);
-
-        // Vérifications
-        assertNotNull(actualPokedex, "The created Pokedex should not be null.");
-        assertEquals(1, actualPokedex.size(), "The Pokedex should contain exactly one Pokemon.");
-        Pokemon retrievedPokemon = actualPokedex.getPokemon(0);
-        assertNotNull(retrievedPokemon, "The retrieved Pokemon should not be null.");
-        assertEquals("Bulbizarre", retrievedPokemon.getName(), "The name of the retrieved Pokemon should be Bulbizarre.");
-        assertEquals(0.75, retrievedPokemon.getIv(), "The IV of the retrieved Pokemon should be 56%.");
+        Pokemon pokemon = pokedex.createPokemon(0, 100, 100, 100, 1);
+        assertNotNull(pokemon, "Pokemon should not be null.");
+        assertEquals("Bulbasaur", pokemon.getName(), "The name should be Bulbasaur.");
+        verify(pokemonFactory).createPokemon(0, 100, 100, 100, 1);
     }
+
+
 
 
 }
