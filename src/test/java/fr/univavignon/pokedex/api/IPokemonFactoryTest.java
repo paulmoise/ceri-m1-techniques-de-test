7e0518072217f36764e664f8685554c24af193ef
paulmoise <m.p.gangbadja@gmail.com>
2024-03-23 14:31:53 +0100

    add badge

--- src/test/java/fr/univavignon/pokedex/api/IPokemonFactoryTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokemonFactoryTest.java
@@ -2,43 +2,47 @@ package fr.univavignon.pokedex.api;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class IPokemonFactoryTest {
 
-    private IPokemonFactory pokemonFactory;
+  private IPokemonFactory pokemonFactory;
 
-    @BeforeEach
-    public void setUp() {
-        pokemonFactory = new IPokemonFactoryImpl();
-    }
+  @BeforeEach
+  public void setUp() {
+    pokemonFactory = new IPokemonFactoryImpl();
+//    pokemonFactory = new RocketPokemonFactory();
+  }
 
-    @Test
-    public void testCreatePokemon() {
-        int index = 15;
-        int cp = 100;
-        int hp = 50;
-        int dust = 100;
-        int candy = 25;
+  @Test
+  public void testCreatePokemon() {
+    int index = 15;
+    int cp = 100;
+    int hp = 50;
+    int dust = 100;
+    int candy = 25;
 
-        String expectedName = "Bulbasaur";
-        int expectedAttack = 55;
-        int expectedDefense = 35;
-        int expectedStamina = 40;
-        double expectedIv = 0.75;
+    String expectedName = "Bulbasaur";
+    int expectedAttack = 55;
+    int expectedDefense = 35;
+    int expectedStamina = 40;
+    double expectedIv = 0.75;
 
-        Pokemon expectedPokemon = new Pokemon(index, expectedName, expectedAttack, expectedDefense, expectedStamina, cp, hp, dust, candy, expectedIv);
+    Pokemon expectedPokemon =
+        new Pokemon(index, expectedName, expectedAttack, expectedDefense, expectedStamina, cp, hp,
+            dust, candy, expectedIv);
 
-        Pokemon actualPokemon = pokemonFactory.createPokemon(index, cp, hp, dust, candy);
+    Pokemon actualPokemon = pokemonFactory.createPokemon(index, cp, hp, dust, candy);
 
-        assertNotNull(actualPokemon);
+    assertNotNull(actualPokemon);
 
-        assertEquals(expectedPokemon.getIndex(), actualPokemon.getIndex());
-        assertEquals(expectedPokemon.getName(), actualPokemon.getName());
-        assertEquals(expectedPokemon.getCp(), actualPokemon.getCp());
-        assertEquals(expectedPokemon.getCandy(), actualPokemon.getCandy());
-        assertEquals(expectedPokemon.getStamina(), actualPokemon.getStamina());
-        assertEquals(expectedPokemon.getDefense(), actualPokemon.getDefense());
-    }
+    assertEquals(expectedPokemon.getIndex(), actualPokemon.getIndex());
+    assertEquals(expectedPokemon.getName(), actualPokemon.getName());
+    assertEquals(expectedPokemon.getCp(), actualPokemon.getCp());
+    assertEquals(expectedPokemon.getCandy(), actualPokemon.getCandy());
+    assertEquals(expectedPokemon.getStamina(), actualPokemon.getStamina());
+    assertEquals(expectedPokemon.getDefense(), actualPokemon.getDefense());
+  }
 }
